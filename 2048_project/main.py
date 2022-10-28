fb0cf2f6998d81bcec0670cf08ce7a3a22fd00a8
Konstantine Gantcev <kanzew@gmail.com>
2022-10-29 01:29:42 +0500

    добавил немного новой логики

--- 2048_project/main.py
+++ 2048_project/main.py
@@ -1,32 +1,77 @@
-import random
+from cgitb import text
+import sys
+import pygame
+from logics import *
 
-def pretty_print(mas):
-    for row in mas:
-        print(*row)
-
-def get_number_from_index(i, j):
-    return i * 4 + j + 1
-
-def get_index_from_number(num):
-    num -= 1
-    x, y = num // 4, num % 4
-    return x, y
-
-def insert_2_of_4(mas, x, y):
-    if random.random() <= 0.75:
-        mas[x][y] = 2
-    else:
-        mas[x][y] = 4
-    return mas    
-
-def get_empty_list(mas):
-    empty = []
-    for i in range(4):
-        for j in range(4):
-            if mas[i][j] == 0:
-                num = get_number_from_index(i, j)
-                empty.append(num)
-    return empty
-
+def draw_interface():
+    pygame.draw.rect(screen, WHITE, TITLE_REC)  # рисуем окно
+    font = pygame.font.SysFont('stxingkai', 70)
+    pretty_print(mas)
+    # отрисовка
+    for row in range(BLOCKS):
+        for column in range(BLOCKS):
+            # находим левые верхние края блоков
+            value = mas[row][column]
+            text = font.render(f'{value}', True, BLACK)
+            w = column * SIZE_BLOCK + (column + 1) * MARGIN
+            h = row * SIZE_BLOCK + (row + 1) * MARGIN + SIZE_BLOCK
+            pygame.draw.rect(screen, COLORS[value], (w, h, SIZE_BLOCK, SIZE_BLOCK))
+            if value != 0:
+                font_w, font_h = text.get_size()
+                text_x = w + (SIZE_BLOCK - font_w) / 2
+                text_y = h + (SIZE_BLOCK - font_h) / 2
+                screen.blit(text, (text_x, text_y))
 
 mas = [ [0] * 4 for i in range(4) ]
+
+COLORS = {
+    0: (130, 130, 130),
+    2: (255, 255, 255),
+    4: (255, 255, 128),
+    8: (255, 255, 0),
+    16: (255, 128, 255),
+    32: (255, 0, 255),
+    64: (255, 0, 128),
+    128: (255, 0, 0),
+    256: (128, 255, 255),
+    512: (128, 255, 128),
+    1024: (128, 255, 0),
+    2048: (128, 128, 255)
+}
+
+WHITE = (255, 255, 255)
+GRAY = (130, 130, 130)
+BLACK = (0, 0, 0)
+BLOCKS = 4
+SIZE_BLOCK = 110
+MARGIN = 10
+WIDTH = BLOCKS * SIZE_BLOCK + (BLOCKS + 1) * MARGIN
+HEIGTH = WIDTH + 110
+TITLE_REC = pygame.Rect(0, 0, WIDTH, 110)
+
+mas[1][2]=2
+mas[3][0]=4
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGTH))
+pygame.display.set_caption('2048')
+draw_interface()
+pygame.display.update()
+while is_zero_in_mas(mas):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                mas = move_left(mas)
+            elif event.key == pygame.K_RIGHT:
+                mas = move_right(mas)
+            empty = get_empty_list(mas)
+            random.shuffle(empty)
+            random_num = empty.pop()
+            x, y = get_index_from_number(random_num)
+            mas = insert_2_of_4(mas, x, y)
+            print(f'Мы заполнили элемент под номером {random_num}')
+            draw_interface()
+            pygame.display.update()
