cb56737da7a26f4bafa6467b68765a7d2d637c74
Konstantine Gantcev <kanzew@gmail.com>
2022-10-29 16:05:08 +0500

    добавил базу, реалзовал всю логику игры

--- 2048_project/logics.py
+++ 2048_project/logics.py
@@ -35,6 +35,7 @@ def get_empty_list(mas):
     return empty
 
 def move_left(mas: list):
+    delta = 0
     for row in mas:
         while 0 in row:
             row.remove(0)
@@ -44,11 +45,13 @@ def move_left(mas: list):
         for j in range(3):
             if mas[i][j] == mas[i][j+1] and mas[i][j] != 0:
                 mas[i][j] *= 2
+                delta += mas[i][j]
                 mas[i].pop(j+1)
                 mas[i].append(0)
-    return mas
+    return mas, delta
 
 def move_right(mas):
+    delta = 0
     for row in mas:
         while 0 in row:
             row.remove(0)
@@ -58,6 +61,35 @@ def move_right(mas):
         for j in range(3, 0, -1):
             if mas[i][j] == mas[i][j - 1] and mas[i][j] != 0:
                 mas[i][j] *= 2
+                delta += mas[i][j]
                 mas[i].pop(j-1)
                 mas[i].insert(0, 0)
-    return mas
+    return mas, delta
+
+def trans(m):
+    res = []
+    for i in range(len(m[0])):
+        buf = []
+        for row in m:
+            buf.append(row[i])
+        res.append(buf)
+    return res
+
+def move_up(mas):
+    mas = trans(mas)
+    mas, delta = move_left(mas)
+    mas = trans(mas)
+    return mas, delta
+
+def move_down(mas):
+    mas = trans(mas)
+    mas, delta = move_right(mas)
+    mas = trans(mas)
+    return mas, delta
+
+def can_move(mas):
+    for i in range(3):
+        for j in range(3):
+            if mas[i][j] == mas[i][j + 1] or mas[i][j] == mas[i + 1][j]:
+                return True
+    return False
--- 2048_project/main.py
+++ 2048_project/main.py
@@ -2,15 +2,27 @@ from cgitb import text
 import sys
 import pygame
 from logics import *
+from database import *
 
-def draw_interface():
-    pygame.draw.rect(screen, WHITE, TITLE_REC)  # рисуем окно
+def draw_top_gamers():
+    pass
+
+def draw_interface(score, delta = 0):
+    pygame.draw.rect(screen, WHITE, TITLE_REC)  
     font = pygame.font.SysFont('stxingkai', 70)
+    font_score = pygame.font.SysFont('simsun', 42)
+    font_delta = pygame.font.SysFont('simsun', 30)
+    text_score = font_score.render('Score: ', True, COLOR_TEXT)
+    text_score_value = font_score.render(f'{score}', True, COLOR_TEXT)
+    screen.blit(text_score, (20, 35))
+    screen.blit(text_score_value, (175, 35))
+    if delta > 0:
+        text_delta = font_delta.render(f' + {delta}', True, COLOR_TEXT)
+        screen.blit(text_delta, (135, 75))
     pretty_print(mas)
-    # отрисовка
+    draw_top_gamers()
     for row in range(BLOCKS):
         for column in range(BLOCKS):
-            # находим левые верхние края блоков
             value = mas[row][column]
             text = font.render(f'{value}', True, BLACK)
             w = column * SIZE_BLOCK + (column + 1) * MARGIN
@@ -24,6 +36,7 @@ def draw_interface():
 
 mas = [ [0] * 4 for i in range(4) ]
 
+COLOR_TEXT = (255, 127, 0)
 COLORS = {
     0: (130, 130, 130),
     2: (255, 255, 255),
@@ -48,6 +61,7 @@ MARGIN = 10
 WIDTH = BLOCKS * SIZE_BLOCK + (BLOCKS + 1) * MARGIN
 HEIGTH = WIDTH + 110
 TITLE_REC = pygame.Rect(0, 0, WIDTH, 110)
+score = 0
 
 mas[1][2]=2
 mas[3][0]=4
@@ -55,23 +69,29 @@ mas[3][0]=4
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGTH))
 pygame.display.set_caption('2048')
-draw_interface()
+draw_interface(score)
 pygame.display.update()
-while is_zero_in_mas(mas):
+while is_zero_in_mas(mas) or can_move(mas):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
         elif event.type == pygame.KEYDOWN:
+            delta = 0
             if event.key == pygame.K_LEFT:
-                mas = move_left(mas)
+                mas, delta = move_left(mas)
             elif event.key == pygame.K_RIGHT:
-                mas = move_right(mas)
+                mas, delta = move_right(mas)
+            elif event.key == pygame.K_UP:
+                mas, delta = move_up(mas)
+            elif event.key == pygame.K_DOWN:
+                mas, delta = move_down(mas)
             empty = get_empty_list(mas)
             random.shuffle(empty)
             random_num = empty.pop()
             x, y = get_index_from_number(random_num)
             mas = insert_2_of_4(mas, x, y)
             print(f'Мы заполнили элемент под номером {random_num}')
-            draw_interface()
+            score += delta
+            draw_interface(score, delta)
             pygame.display.update()
